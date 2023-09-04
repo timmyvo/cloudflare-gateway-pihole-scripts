3cab77ec4cf88d3ca0a7d64dad0395872c89b87f
timmyvo <timmy22021990@yahoo.com>
2023-09-04 16:17:23 +1000

    Update cf_list_create.js

--- cf_list_create.js
+++ cf_list_create.js
@@ -181,7 +181,8 @@ async function createZeroTrustList(name, items, currentItem, totalItems) {
   );
 
   const listId = response.data.result.id;
-  console.log(`Created Zero Trust list`, process.env.CI ? "(redacted on CI)" : `"${name}" with ID ${listId} - ${totalItems - currentItem} left`);
+  // console.log(`Created Zero Trust list`, process.env.CI ? "(redacted on CI)" : `"${name}" with ID ${listId} - ${totalItems - currentItem} left`);
+  console.log(`Created Zero Trust list "${name}" with ID ${listId} - ${totalItems - currentItem} left`);
 }
 
 function percentage(percent, total) {
