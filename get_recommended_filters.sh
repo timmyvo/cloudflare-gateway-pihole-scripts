540f91c7cf29aa07a9f0b494b60bf2a086fc877e
timmyvo <timmy22021990@yahoo.com>
2023-09-06 08:27:56 +1000

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -12,7 +12,7 @@ urls=(
   # https://adaway.org/hosts.txt
   # https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
-  # https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   # https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/gambling-onlydomains.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/light-onlydomains.txt
