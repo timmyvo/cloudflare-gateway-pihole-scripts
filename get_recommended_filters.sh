29efffc3d93bedd6e066af2a6410519ff8a9650e
timmyvo <timmy22021990@yahoo.com>
2023-09-06 07:48:32 +1000

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,7 +1,42 @@
+# #!/bin/bash
+
+# # create an empty input.csv file
+# touch input.csv
+
+# # declare an array of urls
+# urls=(
+#   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
+#   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
+#   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
+#   # https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+#   # https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
+#   # https://adaway.org/hosts.txt
+#   # https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+#   https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
+#   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+#   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
+
+# )
+
+# # loop through the urls and download each file with curl
+# for url in "${urls[@]}"; do
+#   # get the file name from the url
+#   file=$(basename "$url")
+#   # download the file with curl and save it as file.txt
+#   curl -o "$file.txt" "$url"
+#   # append the file contents to input.csv and add a newline
+#   cat "$file.txt" >> input.csv
+#   echo "" >> input.csv
+#   # remove the file.txt
+#   rm "$file.txt"
+# done
+
+# # print a message when done
+# echo "Done. The input.csv file contains merged data from recommended filter lists."
+
 #!/bin/bash
 
-# create an empty input.csv file
-touch input.csv
+source $(dirname "$0")/helpers.sh
 
 # declare an array of urls
 urls=(
@@ -15,21 +50,10 @@ urls=(
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
-
 )
 
-# loop through the urls and download each file with curl
-for url in "${urls[@]}"; do
-  # get the file name from the url
-  file=$(basename "$url")
-  # download the file with curl and save it as file.txt
-  curl -o "$file.txt" "$url"
-  # append the file contents to input.csv and add a newline
-  cat "$file.txt" >> input.csv
-  echo "" >> input.csv
-  # remove the file.txt
-  rm "$file.txt"
-done
+# download all files in parallel and append them to input.csv
+download_lists $urls 'input.csv'
 
 # print a message when done
 echo "Done. The input.csv file contains merged data from recommended filter lists."
