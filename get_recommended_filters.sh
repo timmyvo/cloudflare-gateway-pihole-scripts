620fd0a32868b14d607f3abb8badf3ee5feb1f9a
timmyvo <timmy22021990@yahoo.com>
2023-09-06 07:15:44 +1000

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -14,7 +14,7 @@ urls=(
   # https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
+ https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
 
 )
 
