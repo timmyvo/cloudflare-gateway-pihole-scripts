0a0baa44664caaca9e6a06620b86e21ca3fabd26
timmyvo <timmy22021990@yahoo.com>
2023-09-06 10:05:46 +1000

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -15,7 +15,7 @@ urls=(
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   # https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   # https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
-  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/gambling-onlydomains.txt
+  # https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/gambling-onlydomains.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/light-onlydomains.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/doh-onlydomains.txt
 )
