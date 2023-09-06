c56c717a4827fc449e0d647a1757f5d892427e8b
timmyvo <timmy22021990@yahoo.com>
2023-09-06 11:01:12 +1000

    Update cf_gateway_rule_create.js

--- cf_gateway_rule_create.js
+++ cf_gateway_rule_create.js
@@ -23,63 +23,38 @@ async function getZeroTrustLists() {
 }
 
 ;(async() => {
+      let resp;
       try {
-        const lists = await getZeroTrustLists();
-        const filtered_lists = lists.filter(list => list.name.startsWith('CGPS List'));
-
-        let wirefilter_expression = '';
-
-        // Build the wirefilter expression
-        for (const list of filtered_lists) {
-            wirefilter_expression += `any(dns.domains[*] in \$${list.id}) or `;
-        }
-
-        // Remove the trailing ' or '
-        if (wirefilter_expression.endsWith(' or ')) {
-            wirefilter_expression = wirefilter_expression.slice(0, -4);
-        }
-
-        wirefilter_expression = wirefilter_expression.trim().replace('\n', '');
-
-        if (!process.env.CI) console.log(`Firewall expression contains ${wirefilter_expression.length} characters, and checks against ${filtered_lists.length} filter lists.`)
-
-        console.log('Starting the Cloudflare API request...');
-        const resp = await axios.request({
-            method: 'POST',
-            url: `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules`,
-            headers: {
-                'Authorization': `Bearer ${API_TOKEN}`,
-                'Content-Type': 'application/json',
-                'X-Auth-Email': ACCOUNT_EMAIL,
-                // 'X-Auth-Key': API_TOKEN,
-            },
-            data: {
-                "name": "CGPS Filter Lists",
-                "description": "Filter lists created by Cloudflare Gateway Pi-hole Scripts. Avoid editing this rule. Changing the name of this rule will break the script.",
-                "enabled": true,
-                "action": "block",
-                "filters": ["dns"],
-                "traffic": wirefilter_expression,
-            }
-        });
-        await console.log(resp);
-
-        if (resp) {
-            console.log('Response from API:', resp.data);
-            if (resp.status === 200 && resp.data.success) {
-                console.log('Success:', resp.data.success);
-            } else {
-                console.error('API Request Failed. Status Code:', resp.status);
-                console.error('API Response Data:', resp.data);
-            }
-        } else {
-            console.error('No response received from the API.');
-        }
-        
-    } catch (error) {
-        console.error('Error creating Firewall rule:', error.message);
-        process.exit(1);
-    }
+          resp = await axios.request({
+              method: 'POST',
+              url: `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules`,
+              headers: {
+                  'Authorization': `Bearer ${API_TOKEN}`,
+                  'Content-Type': 'application/json',
+                  'X-Auth-Email': ACCOUNT_EMAIL,
+                  // 'X-Auth-Key': API_TOKEN,
+              },
+              data: {
+                  "name": "CGPS Filter Lists",
+                  "description": "Filter lists created by Cloudflare Gateway Pi-hole Scripts. Avoid editing this rule. Changing the name of this rule will break the script.",
+                  "enabled": true,
+                  "action": "block",
+                  "filters": ["dns"],
+                  "traffic": wirefilter_expression,
+              }
+          });
+      
+          console.log('Response from API:', resp.data);
+      
+          if (resp.status === 200 && resp.data.success) {
+              console.log('Success:', resp.data.success);
+          } else {
+              console.error('API Request Failed. Status Code:', resp.status);
+              console.error('API Response Data:', resp.data);
+          }
+      } catch (error) {
+          console.error('Error making the request:', error.message);
+      }
 })();
 
 //     const lists = await getZeroTrustLists();
