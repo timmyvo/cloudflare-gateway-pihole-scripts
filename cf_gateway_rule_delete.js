a61d987c43ee36b740fa9d1ba981b98ea67dc1a2
timmyvo <timmy22021990@yahoo.com>
2024-06-20 11:25:17 +1000

    Update cf_gateway_rule_delete.js

--- cf_gateway_rule_delete.js
+++ cf_gateway_rule_delete.js
@@ -26,7 +26,7 @@ async function getZeroTrustRules() {
     const rules = await getZeroTrustRules();
     const [filtered_rule] = rules.filter(rule => rule.name === "CGPS Filter Lists");
 
-    if (!filtered_rule) return console.warn("No rule with matching name found - this is not an issue if you haven't run the create script yet. Exiting.");
+    if (!filtered_rule) return console.warn("No rule with matching name found - this is not an issue if you haven't run the create script yet. Exiting.\n");
 
     console.log(`Deleting rule`, process.env.CI ? "(redacted, running in CI)" : `${filtered_rule.name} with ID ${filtered_rule.id}`);
 
