49fb4788c149bc89a292d914439b42f1976b61a9
timmyvo <timmy22021990@yahoo.com>
2024-06-20 12:36:31 +1000

    Update cf_gateway_rule_create.js

--- cf_gateway_rule_create.js
+++ cf_gateway_rule_create.js
@@ -66,7 +66,9 @@ async function getZeroTrustLists() {
     });    
 
     // console.log('Success:', resp.data.success);
-    console.log('\n\nGateway Rule Create Result:\n', resp);
+    console.log('\n\n\n\');
+    console.log();
+    console.log('Gateway Rule Create Result:\n', resp);
 
   } catch (error) {
     console.error('Error:', error.message);
