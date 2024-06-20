90078a799960508271196ea478173b1ff9184d3b
timmyvo <timmy22021990@yahoo.com>
2024-06-20 12:24:12 +1000

    Update cf_gateway_rule_delete.js

--- cf_gateway_rule_delete.js
+++ cf_gateway_rule_delete.js
@@ -22,27 +22,31 @@ async function getZeroTrustRules() {
   return response.data.result;
 }
 
-;(async() => {
+;(async () => {
     const rules = await getZeroTrustRules();
     const [filtered_rule] = rules.filter(rule => rule.name === "CGPS Filter Lists");
 
-    if (!filtered_rule) return console.warn("No rule with matching name found - this is not an issue if you haven't run the create script yet. Exiting.");
-    console.log();
-    console.log(`Deleting rule`, process.env.CI ? "(redacted, running in CI)" : `${filtered_rule.name} with ID ${filtered_rule.id}`);
+    if (filtered_rule) {
+        console.log();
+        console.log("Found CGPS Filter Lists");      
+        console.log(`Deleting rule`, process.env.CI ? "(redacted, running in CI)" : `${filtered_rule.name} with ID ${filtered_rule.id}`);
 
-    const resp = await axios.request({
-        method: 'DELETE',
-        url: `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules/${filtered_rule.id}`,
-        headers: {
-            'Authorization': `Bearer ${API_TOKEN}`,
-            'Content-Type': 'application/json',
-            'X-Auth-Email': ACCOUNT_EMAIL,
-            'X-Auth-Key': API_TOKEN,
-        },
-    });
+        const resp = await axios.request({
+            method: 'DELETE',
+            url: `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules/${filtered_rule.id}`,
+            headers: {
+                'Authorization': `Bearer ${API_TOKEN}`,
+                'Content-Type': 'application/json',
+                'X-Auth-Email': ACCOUNT_EMAIL,
+                'X-Auth-Key': API_TOKEN,
+            },
+        });
 
-    console.log('Success: ', resp.data.success);
-    await sleep(3000); // Cloudflare API rate limit is 1200 requests per 5 minutes, so we sleep for 350ms to be safe
+        console.log('Success: ', resp.data.success);
+        await sleep(3000); // Cloudflare API rate limit is 1200 requests per 5 minutes, so we sleep for 3000ms to be safe
+    } else {
+        console.warn("No rule with matching name found - this is not an issue if you haven't run the create script yet. Exiting.");
+    }
 })();
 
 async function sleep(ms) {
